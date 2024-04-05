009318a3af95c9eb51b0a2dc32b707a5269eba32
Nadine273 <tomns@mail.ru>
2024-04-06 01:25:28 +0200

    exercise 6

--- src/Main.java
+++ src/Main.java
@@ -47,9 +47,41 @@ public class Main {
             System.out.println(" Если возраст ребенка равен "+childAgg+
                     ", то ему можно кататься на аттракционе без сопровождения взрослого");}
         System.out.println("exercise 6");
-        int placeAmount = 102;
-        int sitPlaceAmount = 60;
-        int occupiedPlaceAmount = 83;
+        System.out.println("Вариант 1: При условии, что стоячие места начинаем заполнять после того, " +
+                "как заполнены все сидячие");
+        int placeAmount1 = 102;
+        int sitPlaceAmount1 = 60;
+        int occupiedPlaceAmount1 = 45;
+        int freePlaceAmount1 = placeAmount1 - occupiedPlaceAmount1;
+        if (occupiedPlaceAmount1 < placeAmount1) {
+            System.out.println ("В вагоне есть свободные места;");}
+        else {
+            System.out.println ("В вагоне свободных мест нет");};
+        if (occupiedPlaceAmount1 < sitPlaceAmount1){
+            System.out.println ("сидячие и стоячие");}
+        if (occupiedPlaceAmount1 >= sitPlaceAmount1 && occupiedPlaceAmount1 < placeAmount1) {
+            System.out.println ("стоячие");}
 
+
+        System.out.println("Вариант 2: При условии, что стоячие места заполняются " +
+                "независимо от наличия свободных сидячих мест");
+        int placeAmount2 = 102;
+        int sitPlaceAmount2 = 60;
+        int standPlaceAmount2 = placeAmount2 - sitPlaceAmount2;
+        int occupiedSitPlaceAmount2 = 6;
+        int occupiedStandPlaceAmount2 = 21;
+        int freePlaceAmount2 = placeAmount2 - (occupiedSitPlaceAmount2 + occupiedStandPlaceAmount2);
+        int freeSitPlaceAmount2 = sitPlaceAmount2 - occupiedSitPlaceAmount2;
+        int freeStandPlaceAmount2 = standPlaceAmount2 - occupiedStandPlaceAmount2;
+
+        if (freePlaceAmount2 < placeAmount2) {
+            System.out.println ("В вагоне есть свободные места:");}
+        else {
+            System.out.println ("В вагоне свободных мест нет");};
+        if (occupiedSitPlaceAmount2 < sitPlaceAmount2) {
+            System.out.println (freeSitPlaceAmount2+" свободных сидячих места");}
+        if (occupiedStandPlaceAmount2 < standPlaceAmount2) {
+            System.out.println (freeStandPlaceAmount2+" свободных стоячих места");
+        }
     }
 }
